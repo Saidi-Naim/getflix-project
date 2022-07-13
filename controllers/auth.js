51412a741319e95fe3af8a9057e36d044820890f
fel <felicien.dehertogh@protonmail.com>
2022-07-13 14:27:28 +0200

    Update .env

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,6 +9,14 @@ const db = mysql.createConnection({
   database: process.env.DATABASE,
 });
 
+db.connect((err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('MYSQL connected...')
+  }
+})
+
 exports.register = (req, res) => {
   const { email, password, confirmPassword } = req.body;
 
