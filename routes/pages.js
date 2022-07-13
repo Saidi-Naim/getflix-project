912cf4eb3f803f51f8fd9f2c2b4b08a85998c59d
elmiri younes <elmiri.younes@hotmail.com>
2022-07-13 12:46:28 +0200

    implementation of authentification: login/register/logout

--- routes/pages.js
+++ routes/pages.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-
 router.get("/", (req, res) => {
-    res.render("index");
+  res.render("index", { logged: req.cookies.loggedin });
 });
-
 
 router.get("/login", (req, res) => {
   res.render("login");
