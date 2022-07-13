789fbf9e5afae93c0ebcbf4fdb8bf54b84d6515d
fel <felicien.dehertogh@protonmail.com>
2022-07-13 09:56:32 +0200

    Update .env

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
+const mysql = require("mysql");
 
 dotenv.config({ path: "./.env" });
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+// Connect with db
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
+})
+
+db.connect((err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('MYSQL connected...')
+  }
+})
 
 // to use frontend: files css, assets etc with nodejs server
 const publicDirectory = path.join(__dirname, "./public");
