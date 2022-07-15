de7add177e2e9f96dcb0f082c1bc38d92920bb79
AIchrak <ichrak.aissia@outlook.com>
2022-07-15 15:08:28 +0200

    update

--- routes/pages.js
+++ routes/pages.js
@@ -12,5 +12,8 @@ router.get("/login", (req, res) => {
 router.get("/register", (req, res) => {
   res.render("register");
 });
+router.get("/search", (req, res) => {
+  res.render("search");
+});
 
 module.exports = router;
